e492513bf2c51e85a665517248ab0fa57614b20d
Hikaru Belzer <114829186+hikbel@users.noreply.github.com>
2023-07-22 14:13:22 -0400

    Update and rename reading_in_csv.h to sorter_and_summary_generator.h

--- sorter_and_summary_generator.h
+++ sorter_and_summary_generator.h
@@ -1,16 +1,16 @@
-/************************************************
-** File:    reading_in_csv.h
-** Project: Data Summary
+/******************************************************************************
+** File:    sorter_and_summary_generator.h
+** Project: CSV Sorter and Summary Generator
 ** Author:  Hikaru Belzer
-** Date:    7/1/2023
+** Date:    7/22/2023
 **
-** This is the header file, which contains the
-** constants, function prototypes, and other
-** generic information.
+** This is the header file, which contains the libraries, namespace, constants,
+** and function prototypes.
 **
 ** To compile: g++ -Wall reading_in_csv.cpp
 ** To run: ./reading_in_csv
-************************************************/
+**
+******************************************************************************/
 
 #include <iostream>
 #include <fstream>
@@ -26,9 +26,10 @@ using namespace std;
 // Constants
 const string EMPTY_STRING = "";
 const char SPACE_CHAR = ' ';
+const int WIDTH = 5;
 
 // Function Prototypes
-void loadFile(vector<int>& vectorOfNums);
+int loadFile(vector<int>& vectorOfNums);
 int howToSort();
 void writingToFile(const vector<int>& vectorOfNums);
 void sorting(int choice, vector<int>& vectorOfNums);
